92ed7fcd687e318c43d68936965482898f6ee0ba
izabella-m <izabella.pessoa03@oulook.com>
2024-01-04 16:23:11 -0300

    Header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+      <div class="mt-10 footer-content">
+        <p>&copy; 2024 Minha Empresa</p>
+        <v-card width="600"></v-card>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
+  <style scoped>
+  footer {
+    background-color: #34495e;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+  }
+  </style>
+  
