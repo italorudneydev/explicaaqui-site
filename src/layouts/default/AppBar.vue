bc3cb8a13b73769809daaaad953cbc4a6523ee14
izabella-m <izabella.pessoa03@oulook.com>
2024-01-04 16:24:32 -0300

    Header

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,11 +1,45 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-
-    </v-app-bar-title>
+    <div class="divPrincipal d-flex">
+      <div class="me-auto">
+        <v-img 
+          class="spacingImg"
+          :width="200"
+          :height="38"
+          src="/src/assets/Logo-padrão.png"
+        ></v-img>
+      </div>
+      <div>
+        <v-btn variant="text">BENEFÍCIOS</v-btn>
+        <v-btn variant="text">COMO FUNCIONA</v-btn>
+        <v-btn variant="text">PLANOS</v-btn>
+        <v-btn variant="text">FAQ</v-btn>
+        <v-btn variant="outlined" rounded="xl">Cadastre-se agora</v-btn>
+      </div>
+    </div>
   </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
 </script>
+
+<style>
+
+.divPrincipal {
+  margin-left: 15%;
+  margin-right: 15%;
+}
+
+.spacingImg {
+  margin-right: 400px;
+
+}
+
+</style>
