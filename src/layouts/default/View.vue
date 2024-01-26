9e332de989b1b55803ee18d6d527e7e0f80a8c47
izabella-m <izabella.pessoa03@oulook.com>
2024-01-26 15:23:50 -0300

    Finals adjustments of footer

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,31 +1,37 @@
 <template>
     <router-view />
     <v-card class="footer">
-      <div class="divPrincipal">
-        <div class="d-flex flex-row firstLine">
-          <v-img 
-          clas="me-auto"
-            :width="166"
-            :height="32"
-            src="/src/assets/Logo-padrão.png"
-          ></v-img>
-          <v-btn class="spacingBtn" variant="text">CADASTRE-SE AGORA</v-btn>
-        </div>
-        <v-divider></v-divider>
-        <div class="d-flex flex-row secondLine">
+      <div class="justify-center spacingPrincipalNavigation">
+        <div class="d-flex align-center flex-row secondLine">
           <div class="me-auto d-flex">
-            <v-btn variant="text" size="x-large" src="/" icon="mdi-instagram"></v-btn>
-            <v-btn variant="text" size="x-large" src="/" icon="mdi-facebook"></v-btn>
-            <v-btn icon size="x-large" variant="text">
+            <v-img 
+              :width="150"
+              :height="32"
+              src="/src/assets/Logo-padrao.png"
+              class="mb-6 mt-3"
+            ></v-img>
+          </div>
+          <div class="d-flex flex-row mt-n3">
+            <v-btn class="spacingBtn" to="/" variant="text">CADASTRE-SE AGORA</v-btn>
+          </div>
+        </div>
+
+        <v-divider class="mx-3"></v-divider>
+
+        <div class="d-flex align-center flex-row secondLine">
+          <div class="me-auto d-flex">
+            <v-btn class="spacingIconsSmallGrid" variant="text" size="x-large" src="/" icon="mdi-instagram"></v-btn>
+            <v-btn class="spacingIconsSmallGrid" variant="text" size="x-large" src="/" icon="mdi-facebook"></v-btn>
+            <v-btn icon size="x-large" class="spacingIconsSmallGrid" variant="text">
               <v-avatar>
                 <v-img 
-                  class="twtter"
+                  class="twitter"
                   src="/src/assets/twitter-x.png"
                 ></v-img>
               </v-avatar>
             </v-btn>
           </div>
-          <div class="d-flex flex-row">
+          <div class="d-flex flex-row mt-n3">
             <v-sheet v-for="(itemDownload, index) in itemsDownload" :key="index" :height="40" color="#FFFFFF" class="mt-4 mr-4" :width="114" rounded>
               <div class="d-flex">
                 <v-img 
@@ -59,31 +65,44 @@ export default {
 }
 </script>
 
-
 <style scoped>
+
+@media screen and (max-width: 959px) {
+  .spacingPrincipalNavigation {
+    margin-top: 15px;
+  }
+}
+  
+@media screen and (min-width: 960px) {
+  .spacingPrincipalNavigation {
+    margin-left: 150px;
+    margin-right: 10%;
+    margin-top: 18px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .spacingIconsSmallGrid {
+    margin-right: -10px !important;
+    margin-left: -20px;
+  }
+}
 .footer {
   background-color: #4E42FF0D;
   height: 200px;
 }
-.divPrincipal {
-  margin-left: 20%;
-  margin-right: 20%;
+.align-center {
+  align-items: center;
 }
-.firstLine {
-  margin-top: 25px;
-  margin-bottom: 25px;
-}
-.spacingBtn {
-  font-weight: 600;
-}
-
 .secondLine {
   margin-top: 15px;
   margin-left: 12px;
 }
-.imgTwitter {
-  padding-top: 2px;
-  margin-left: -105px;
+.twitter {
+  width: 10px;
+}
+.spacingBtn {
+  font-weight: 600;
 }
 .description1 {
   font-size: 8px;
@@ -94,10 +113,6 @@ export default {
   font-size: 12px;
   font-weight: 600;
   margin-left: -35px;
-}
-
-.twtter {
-  width: 10px;
 }
 
 </style>
