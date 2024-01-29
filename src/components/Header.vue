9abc7261a0be4f25986472c837b3840473aac72b
izabella-m <izabella.pessoa03@oulook.com>
2024-01-29 16:22:21 -0300

    Additional adjustments of header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,105 +1,104 @@
 <template>
-  <v-main>
-    <v-card style="z-index: auto;" height="72px">
-      <div class="justify-center d-flex spacingPrincipalNavigation">
-        <div class="me-auto d-none d-md-flex">
-          <v-img 
-            :width="200"
-            :height="38"
-            src="/src/assets/Logo-padrao.png"
-          ></v-img>
-        </div>
-        <div class="d-none d-md-flex">
-          <v-btn 
-            variant="plain"
-            color="#000000"
-            v-for="(item, index) in items"
-            :key="index"
-            @click="redirectToLink(item.link)"
-          >{{ item.title }}</v-btn>
-          <v-btn variant="outlined" rounded="xl">Cadastre-se agora</v-btn>
-        </div>
-        <!-- Tela menor -->
-        <v-navigation-drawer class="w-100" temporary v-model="drawer" location="right" hide-overlay @resize="checkWindowSize">
-          <v-list class="d-flex mx-auto">
-            <v-img
-              src="/src/assets/Logo-icone.png"
-              :height="60"
-              class="d-flex dimensionImage"
-            ></v-img>
-            <v-spacer></v-spacer>
-            <v-btn
-              variant="plain"
-              class="mt-3"
-              @click.stop="drawer = !drawer"
-            >
-              <v-icon size="30">mdi-window-close</v-icon>
-            </v-btn>
-          </v-list>
+<v-main overflow-y:auto>
+    <v-navigation-drawer  class="w-100" temporary location="right" hide-overlay @resize="checkWindowSize" v-model="drawer"> <!--dentro do menu-->
+      <v-list class="d-flex mx-auto"> <!--menu pequeno-->
+        <v-img
+          src="/src/assets/Logo-icone.png"
+          :height="60"
+          class="d-flex dimensionImage"
+        ></v-img>
+        <v-spacer></v-spacer>
+        <v-btn
+          variant="plain"
+          class="mt-3"
+          @click.stop="drawer = !drawer"
+        >
+          <v-icon size="30">mdi-window-close</v-icon>
+        </v-btn>
+      </v-list>
 
-          <v-divider class="mb-2"></v-divider>
+      <v-divider class="mb-2"></v-divider>
 
-          <v-list
-            v-for="(item, index) in items"
-            :key="index"
-          >
-            <v-list-item
-              variant="plain"
-              @click="redirectToLink(item.link)"
-              class="pb-8 pt-10 mt-n4 ml-6 mr-6"
-            >{{ item.title }} 
-            </v-list-item>
-            <v-divider class="mx-6 mb-4 mt-0 mb-n2" :thickness="2"></v-divider>
-          </v-list>
+      <v-list
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        <v-list-item
+          variant="plain"
+          @click="redirectToLink(item.link)"
+          class="pb-8 pt-10 mt-n4 ml-6 mr-6"
+        >{{ item.title }} 
+        </v-list-item>
+        <v-divider class="mx-6 mb-4 mt-0 mb-n2" :thickness="2"></v-divider>
+      </v-list>
 
-          <div class="d-flex flex-column mt-6 align-center">
-            <v-text-field
-              class="emailInputNavigation"
-              density="compact"
-              variant="outlined"
-              rounded="xl"
-              placeholder="Insira seu e-mail"
-            >
-            </v-text-field>
-            <v-btn 
-              rounded="xl" 
-              class="btnRegisterNavigation"
-            ><b>CADASTRE-SE AGORA</b>
-            </v-btn>
-          </div>
-        </v-navigation-drawer>
-        
-        <div class="d-sm-flex mx-auto d-md-none"> <!-- logo pequena sem menu-->
-          <v-img 
-            :width="100"
-            :height="38"
-            src="/src/assets/Logo-icone.png"
-          ></v-img> 
-        </div>
-        <v-spacer class="d-sm-flex d-md-none"></v-spacer>
-        <v-btn variant="outlined" rounded="xl" class="d-sm-flex d-md-none">Cadastre-se agora</v-btn>
-        <v-btn variant="plain" class="iconMenu d-sm-flex d-md-none" @click.stop="drawer = !drawer">
-          <v-icon size="30">mdi-menu</v-icon>
+      <div class="d-flex flex-column mt-6 align-center">
+        <v-text-field
+          class="emailInputNavigation"
+          density="compact"
+          variant="outlined"
+          rounded="xl"
+          placeholder="Insira seu e-mail"
+        >
+        </v-text-field>
+        <v-btn 
+          rounded="xl" 
+          class="btnRegisterNavigation"
+          @click="scrollToBenefícios()"
+
+        ><b>CADASTRE-SE AGORA</b>
         </v-btn>
       </div>
-    </v-card>
+    </v-navigation-drawer>
+
+    <v-app-bar :elevation="0">
+      <div class="me-auto d-none d-md-flex">
+        <v-img 
+          :width="200"
+          :height="38"
+          src="/src/assets/Logo-padrao.png"
+        ></v-img>
+      </div>
+      <div class="d-none d-md-flex">
+        <v-btn 
+          variant="plain"
+          color="#000000"
+          v-for="(item, index) in items"
+          :key="index"
+        >{{ item.title }}</v-btn>
+        <v-btn variant="outlined"          
+        rounded="xl">Cadastre-se agora</v-btn>
+      </div>
+      <div class="d-sm-flex mx-auto d-md-none"> 
+        <v-img 
+          :width="100"
+          :height="38"
+          src="/src/assets/Logo-icone.png"
+        ></v-img> 
+      </div>
+      <v-spacer class="d-sm-flex d-md-none"></v-spacer>
+      <v-btn variant="outlined" rounded="xl" class="d-sm-flex d-md-none">Cadastre-se agora</v-btn>
+      <v-btn variant="plain" class="iconMenu d-sm-flex d-md-none" @click.stop="drawer = !drawer">
+        <v-icon size="30">mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <LandingPage/>
   </v-main>
 </template>
-  
-<script>
 
+<script>
 export default {
   data() {
     return {
-      drawer: null,
-      rail: true,
+      drawer: false,
       items: [
         { title: 'BENEFÍCIOS', link: '/' },
         { title: 'COMO FUNCIONA', link: '/' },
         { title: 'PLANOS', link: '/' },
         { title: 'FAQ', link: '/' },
       ],
-    }
+    };
   },
   methods: {
     // Checa tamanho da tela para fechar o navigation automamticamente se acima de 959px
@@ -107,7 +106,11 @@ export default {
       if (window.innerWidth > 959) {
         this.drawer = false;
       }
-    }
+    },
+    scrollToBenefícios() {
+      // y: 1080 é a posição vertical da sessão "benefícios"
+      document.querySelector("body").scrollTo(0, 1080);
+    },
   },
   watch: {
     drawer(value) {
@@ -121,10 +124,10 @@ export default {
   beforeDestroy() {
     window.removeEventListener('resize', this.checkWindowSize);
   }
-}
+};
 
 </script>
-  
+
 <style>
 
 @media screen and (max-width: 959px) {
@@ -168,10 +171,4 @@ export default {
 .align-center {
   align-items: center;
 }
-
-.drawer-on-top {
-    z-index: 1; /* Adjust as needed */
-}
-
 </style>
-  
